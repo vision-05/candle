4f99039fc927f88a08b77a63755f67b1845720bb
vision-05 <t.frankel@outlook.com>
2025-11-12 21:14:35 +0000

    properly implemented mini batches and added optimiser skeletons

--- nn.py
+++ nn.py
@@ -28,7 +28,7 @@ class nn:
             else:
                 self.fns.append(arg)
 
-        self.dJdA = lambda A, Y: A-Y #for 2 special cases not including linear regression
+        self.dJdZ = lambda A, Y: A-Y #for 2 special cases not including linear regression
 
     def forward(self, X):
         res = X
@@ -44,7 +44,7 @@ class nn:
         return res
 
     def backward(self, Y_pred, Y):
-        dZ = self.dJdA(Y_pred, Y)
+        dZ = self.dJdZ(Y_pred, Y)
         for fn in reversed(self.fns[:-1]): #first grad already calculated
             dZ = fn.backward(dZ)
 
@@ -58,29 +58,38 @@ class nn:
     def train(self, X, Y, X_val, Y_val, no_epochs, learning_rate):
         print("Training")
         batch_size = 500
-        no_batches = np.floor(X.shape[1]/batch_size)
-        cur_batch = 0
-        for i in range(no_epochs):
-            if no_batches == cur_batch:
-                cur_batch = 0
-            batch_X = X[:,batch_size*cur_batch:batch_size*(cur_batch+1)]
-            batch_Y = Y[:,batch_size*cur_batch:batch_size*(cur_batch+1)]
-            batch_Y_pred = self.forward(batch_X)
 
-            cost = self.cost_fn.compute_cost(batch_Y_pred, batch_Y)
+        m = X.shape[1]
+        for i in range(no_epochs):
+            epoch_cost = 0
+            permutation = np.random.permutation(m)
+
+            X_shuffled = X[:, permutation]
+            Y_shuffled = Y[:, permutation]
+
+            no_batches = m//batch_size
+
+            for j in range(no_batches):
+                batch_X = X[:,batch_size*j:batch_size*(j+1)]
+                batch_Y = Y[:,batch_size*j:batch_size*(j+1)]
+                batch_Y_pred = self.forward(batch_X)
+
+                cost = self.cost_fn.compute_cost(batch_Y_pred, batch_Y)
+                epoch_cost += cost
+
+                self.backward(batch_Y_pred, batch_Y)
+
+                for fn in self.fns:
+                    if isinstance(fn, activations.Linear):
+                        fn.weights -= learning_rate * fn.dW
+                        fn.biases -= learning_rate * fn.dB
 
             if i % 10 == 0 or i == no_epochs - 1:
                 Y_val_pred = self.predict(X_val)
+                Y_train_pred = self.predict(X)
+                Y_train_acc = self.accuracy(Y_train_pred, Y)
                 Y_val_cost = self.cost_fn.compute_cost(Y_val_pred, Y_val)
                 Y_val_acc = self.accuracy(Y_val_pred, Y_val)
-                print(f"Epoch {i}: cost {cost}")
-                print(f"Validation cost {Y_val_cost}, Accuracy {Y_val_acc*100:.2f}%")
+                print(f"Epoch {i}: training cost {epoch_cost/no_batches}, validation cost {Y_val_cost}")
+                print(f"Validation accuracy {Y_val_acc*100:.2f}%, training Accuracy {Y_train_acc*100:.2f}%")
 
-            self.backward(batch_Y_pred, batch_Y)
-
-            for fn in self.fns:
-                if isinstance(fn, activations.Linear):
-                    fn.weights -= learning_rate * fn.dW
-                    fn.biases -= learning_rate * fn.dB
-
-            cur_batch += 1
---
+++ optimisers.py
@@ -0,0 +1,19 @@
+class Optimiser:
+    def __init__(self):
+        pass
+
+class SGD(Optimiser):
+    def __init__(self):
+        super().__init__()
+
+class EWMA(Optimiser):
+    def __init__(self):
+        super().__init__()
+
+class RMSProp(Optimiser):
+    def __init__(self):
+        super().__init__()
+
+class ADAM(Optimiser):
+    def __init__(self):
+        super().__init__()
